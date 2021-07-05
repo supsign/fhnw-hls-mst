d41c980229e79aee15a182abb6dd0670bd04d5cf
Florian Ratz <florian.ratz@supsign.ch>
2021-07-05 15:11:48 +0200

    no message

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,19 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Importers\CourseImporter;
-use App\Importers\CourseGroupImporter;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function test()
     {
-        (new CourseImporter)
-            ->setDirectory(storage_path().'/app/')
-            ->setFileName('modul.csv')
-            ->import();
-
         return view('welcome');
     }
 }

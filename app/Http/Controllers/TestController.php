e7d34bb12be91c1cab87d46e980ea58afccfd505
Florian Ratz <florian.ratz@supsign.ch>
2021-07-06 15:36:11 +0200

    no message

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,25 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Assessment;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
-{
-    public function test()
     {
-        $assessment = Assessment::create([
-            'course_id' => 1,
-            'study_field_id' => 13,
-            'start_semester_id' => 1
-        ]);
-
-        var_dump(
-            $assessment->studyField()->first(),
-            $assessment->studyField()->first()->id === $assessment->id
-        );
-
-
         return view('welcome');
     }
 }

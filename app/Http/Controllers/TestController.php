728ded7906c3beeffe43dd6c81c05040205edeaf
Florian Ratz <florian.ratz@supsign.ch>
2021-07-08 09:26:08 +0200

    no message

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Importers\SkillPrerequisiteImporter;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function test()
     {
-        (new SkillPrerequisiteImporter)->import();
-
         return view('welcome');
     }
 }

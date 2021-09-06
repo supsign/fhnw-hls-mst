10021865f10b33c2b1eb00394a6170d59c8d3f34
Florian Ratz <florian.ratz@supsign.ch>
2021-09-06 15:51:51 +0200

    check header of file before parsing

--- app/Imports/StudyFieldImport.php
+++ app/Imports/StudyFieldImport.php
@@ -4,6 +4,7 @@ namespace App\Imports;
 
 use App\Models\StudyField;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 /*
 usage:
@@ -13,7 +14,7 @@ use App\Imports\StudyFieldImport;
 $excel->import(new StudyFieldImport, 'Tab1_Studiengang.xlsx');
 */
 
-class StudyFieldImport implements ToModel
+class StudyFieldImport implements ToModel, WithHeadingRow
 {
     /**
     * @param array $row
@@ -22,17 +23,18 @@ class StudyFieldImport implements ToModel
     */
     public function model(array $row): void
     {
-        if (!is_int($row[0])) {
+        if (count(array_diff(['id_anlass', 'anlassnummer', 'anlassbezeichnung'], array_keys($row))) !== 0) {
+            //  Throw error? Write log?
             return;
         }
 
         $studyField = StudyField::updateOrCreate(
-            ['evento_id' => $row[0]], 
-            ['evento_number' => $row[1]],
+            ['evento_id' => $row['id_anlass']], 
+            ['evento_number' => $row['anlassnummer']],
         );
 
         if (!$studyField->name) {
-            $studyField->name = $row[2];
+            $studyField->name = $row['anlassbezeichnung'];
             $studyField->save();
         }
     }

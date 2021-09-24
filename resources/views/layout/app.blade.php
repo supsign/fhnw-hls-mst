07b1871c5c96ed4538c496448de1041a1adb85d8
Sil3nce72 <nb.guen@hotmail.com>
2021-09-24 13:06:09 +0200

    app blade ueberarbeitet

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,24 +8,21 @@
 
         <script src="{{ asset('js/app.js') }}" defer></script>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="{{ asset('vendors/fontawesome-pro/css/all.min.css') }}">
 
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
     </head>
     <body class="font-body">
+    <div id="app" class="w-full bg-blend-darken ">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             <x-layout.top/>
             <x-layout.header/>
-            <div>
+            <div id="main" class="flex-grow">
                 {{ $slot }}
             </div>
+            <x-layout.footer/>
+            <x-layout.bottom/>
         </div>
+    </div>
     </body>
 </html>

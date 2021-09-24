e374febec2d50b7eba829a7e9a8d42cba3fb9273
Sil3nce72 <nb.guen@hotmail.com>
2021-09-24 16:56:30 +0200

    struktur erweitert, schedules

--- resources/views/components/user/my-schedules.blade.php
+++ resources/views/components/user/my-schedules.blade.php
@@ -6,7 +6,7 @@
 
         <div class="flex flex-col">
             @for ($i = 1; $i < 6; $i++)
-                <div class="">Stundenplan {{$i}}</div>
+                <div class="p-2 bg-gray-100 mb-2 rounded">Stundenplan {{$i}}</div>
             @endfor
         </div>
     </x-app.card>

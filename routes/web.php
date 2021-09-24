f636c598c829197c884674df5d392390f306124e
Sil3nce72 <nb.guen@hotmail.com>
2021-09-24 09:00:26 +0200

    home-route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login', [LoginController::class, 'login'])->name('post.auth.login');
 
-Route::get('', [TestController::class, 'test'])->name('home');
+Route::get('', [TestController::class, 'test'])->name('test');
+Route::get('/', [HomeController::class, 'index'])->name('home');

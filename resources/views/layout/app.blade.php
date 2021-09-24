8c511642a4f9522924b06a1c9eee70c87d66ce83
Sil3nce72 <nb.guen@hotmail.com>
2021-09-24 13:17:17 +0200

    anpassungen app.blade

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="robots" content="{{ isset($robots) ? $robots : 'noindex,nofollow' }}"/>
+        <meta name="description" content="{{ isset($description) ? $description : '' }}">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="theme-color" content="#ffffff">
+        <meta name="language" content="{{ $locale }}">
 
         <title>{{ str_replace('<br>', ',', $title) }} | HLS MST</title>
 

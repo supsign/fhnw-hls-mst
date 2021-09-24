cae4586801ee389f8c0709a64de1799ba3e625da
Fabrizio Cocco <fabrizio.cocco@supsign.ch>
2021-09-24 17:14:14 +0200

    wip

---
+++ app/Services/User/PermissionService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services\User;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class PermissionService
+{
+    public function assignStudent(User $user): User
+    {
+        $user->assignRole('student');
+        return $user;
+    }
+
+    public function removeStudent(User $user): User
+    {
+        $user->removeRole('student');
+        return $user;
+    }
+
+
+    public function canShowAppOrAbort(): self
+    {
+        if (!Auth::user()->hasPermissionTo('show app')) {
+            abort(403, __('l.noAccess'));
+        }
+
+        return $this;
+    }
+
+    public function canManageBackendOrAbort(): self
+    {
+        if (!Auth::user()->hasPermissionTo('manage backend')) {
+            abort(403, __('l.noAccess'));
+        }
+
+        return $this;
+    }
+
+    public function canPlanScheduleOrAbort(): self
+    {
+        if (!Auth::user()->hasPermissionTo('plan schedule')) {
+            abort(403, __('l.noAccess'));
+        }
+
+        return $this;
+    }
+}
---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Create Roles
+        $student = Role::create(['name' => 'student']);
+        $mentor = Role::create(['name' => 'mentor']);
+        $appAdmin = Role::create(['name' => 'app-admin']);
+        Role::create(['name' => 'server-admin']);
+
+        // Create Permissions
+        Permission::create(['name' => 'show app']);
+        Permission::create(['name' => 'show backend']);
+        Permission::create(['name' => 'plan schedule']);
+        Permission::create(['name' => 'manage backend']);
+
+        $appAdmin->givePermissionTo('show app', 'show backend', 'manage backend');
+        $mentor->givePermissionTo('show app');
+        $student->givePermissionTo('show app', 'plan schedule');
+    }
+}

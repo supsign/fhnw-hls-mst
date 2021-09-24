4a2873549b2d7d9c9d924ff878bc39f0ec860341
Sil3nce72 <nb.guen@hotmail.com>
2021-09-24 09:10:53 +0200

    vue app.ts anpassungen

--- resources/vue/app.ts
+++ resources/vue/app.ts
@@ -4,3 +4,18 @@ import { Vue } from "vue-property-decorator";
 import Vuex from "vuex";
 
 Vue.use( Vuex );
+
+const store = new Vuex.Store({
+    strict: true,
+    modules: {
+
+    },
+});
+
+const app = new Vue({
+    el: "#app",
+    components: {
+
+    },
+    store
+});

f2c43cc161e21d8c6e5d836b30b62979da9e82ff
Sil3nce72 <nb.guen@hotmail.com>
2021-09-27 11:23:48 +0200

    footer hinzugefügt

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Antoninus -->
-</div>
+<footer class="p-3 mt-auto text-xs text-white bg-gray-700">
+    <div class="text-center sm:flex sm:justify-between">
+        <a href="https://supsign.ch" target="_blank" rel="noopener noreferrer" class="block mb-2 sm:mb-0">
+            &copy; 2021 Supsign GmbH
+        </a>
+
+        <div class="flex justify-center space-x-4">
+            <a href="" target="_blank">
+                Impressum
+            </a>
+            <div> &bull;
+            </div>
+            <a href="https://ol-events.zendesk.com/hc/de/sections/360002778197-Changelog" target="_blank" rel="noopener noreferrer">
+                Version 2.2.0
+            </a>
+        </div>
+    </div>
+</footer>

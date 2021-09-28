6f5bda27fb0352650c0e8963db68091e15861795
Florian Ratz <florian.ratz@supsign.ch>
2021-09-28 16:41:33 +0200

    commit test file

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Imports\StudyFieldImport;
+use App\Imports\StudyFieldYearImport;
+use Maatwebsite\Excel\Excel;
+
 class TestController extends Controller
 {
-    public function test()
+    public function test(Excel $excel)
     {
+        $excel->import(new StudyFieldImport, 'Tab1_Studiengang.xlsx');
+        $excel->import(new StudyFieldYearImport(), 'Tab2_Studienjahrgang.xlsx');
+
+        echo 'done';
     }
 }

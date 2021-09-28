24806d13bd9282f5c055b4f618564136c4e0ca17
Sil3nce72 <nb.guen@hotmail.com>
2021-09-28 09:29:50 +0200

    lang files ueberarbeitet

--- resources/lang/de-CH/l.php
+++ resources/lang/de-CH/l.php
@@ -1,7 +1,12 @@
 <?php
 
 return [
-
-    'userName' => 'Name',
-
+    'userData' => 'Benutzerdaten',
+    'userName' => 'Name:',
+    'userRole' => 'Rolle:',
+    'userStudyCourse' => 'Studiengang:',
+    'userStudyField' => 'Studienrichtung:',
+    'schedule' => 'Stundenplan',
+    'scheduleCurrent' => 'aktuelle Stundenplanung',
+    'allSchedules' => 'Alle',
 ];

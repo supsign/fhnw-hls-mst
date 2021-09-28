3c036d765b366e4963183ee696ce0340e54b79ae
Sil3nce72 <nb.guen@hotmail.com>
2021-09-28 11:18:39 +0200

    add schedule angepasst - fixed/absolute

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,10 +6,10 @@
     <div class="container p-3 mx-auto">
         <x-user.me></x-user.me>
         <x-user.my-schedules></x-user.my-schedules>
-        <div class="fixed z-30 cursor-pointer right-5 bottom-14">
-            <a href="{{ route('schedule.new') }}" class="button-primary m-3 text-sm">
-                <i class="fa fa-plus" aria-hidden="true"></i>
-            </a>
-        </div>
+    </div>
+    <div class="fixed z-30 cursor-pointer right-5 bottom-14">
+        <a href="{{ route('schedule.new') }}" class="button-primary m-3 text-sm">
+            <i class="fa fa-plus" aria-hidden="true"></i>
+        </a>
     </div>
 </x-layout.app>

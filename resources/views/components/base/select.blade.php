4367a0a7634d64093964e20e257c5aab6422cfd7
Sil3nce72 <nb.guen@hotmail.com>
2021-09-29 10:17:25 +0200

    select grundlegend fertiggestellt

--- resources/views/components/base/select.blade.php
+++ resources/views/components/base/select.blade.php
@@ -1,4 +1,9 @@
-<vue-select
-
-/>
-
+<vue-select class="w-full mt-2 min-h-16"
+            :init-value="{{ $value ?? json_encode(null) }}"
+            name="{{ $attributes->get('name') }}"
+            {{ $attributes->get('clearable') ? 'clearable' : '' }}
+            {{ $attributes->get('searchable') ? 'searchable' : '' }}
+            {{ $attributes->get('required') ? 'required' : '' }}
+            {{ $attributes->get('disabled') ? 'disabled' : '' }}
+            is-blade>
+</vue-select>
--- resources/vue/components/form/vueSelect.vue
+++ resources/vue/components/form/vueSelect.vue
@@ -18,13 +18,8 @@
             @search="search"
             :disabled="disabled"
             :selectable="selectable"
-            :filter="getFilter()"
-            :class="{
-                'is-invalid': showError
-            }"
             @search:blur="blur"
         >
-            <div slot="no-options">{{ lang("l.nothingFound") }}</div>
             <template #option="option" v-if="showOption">
                 {{ showOption(option) }}
             </template>
@@ -32,14 +27,16 @@
                 {{ showOption(option) }}
             </template>
         </v-select>
-        <div class="text-xs vs__error" role="alert" v-if="showError">
-            {{ initialError || fieldControl.errors[0] }}
-        </div>
         <select :name="name" hidden>
             <option
                 :value="internalValue.id"
                 selected
                 v-if="internalValue"
+            ></option>
+            <option
+                :value="null"
+                selected
+                v-else
             ></option>
         </select>
 
@@ -50,9 +47,10 @@
 </template>
 
 <script lang="ts">
-import { Component } from "vue-property-decorator";
+import {Component, Emit, Prop} from "vue-property-decorator";
 import BaseComponent from "../base/baseComponent";
 import vSelect from "vue-select";
+import {IModel} from "../../store/model.interface";
 
 @Component({
     components: {
@@ -60,6 +58,135 @@ import vSelect from "vue-select";
     }
 })
 export default class VueSelect extends BaseComponent {
+    @Prop({
+        type: [Object, String]
+    })
+    value: IModel | string;
 
+    @Prop({
+        type: Object
+    })
+    initValue: IModel;
+
+    @Prop({
+        type: Boolean
+    })
+    isBlade: boolean;
+
+    public input(ev: any) {
+        console.log(ev);
+        this.internalValue = ev;
+        this.initialError = null;
+        this.$emit("input", ev);
+        this.blur(ev);
+    }
+
+    @Prop({
+        type: String,
+        default: "id"
+    })
+    optionKey: string;
+
+    @Prop({
+        type: String,
+        default: ""
+    })
+    name: string;
+
+    @Prop({
+        type: String,
+        default: ""
+    })
+    label: string;
+
+    @Prop({
+        type: Array
+    })
+    searchKeys: string[];
+
+    @Prop({
+        type: Boolean,
+        default: false
+    })
+    clearable: boolean;
+
+    @Prop({
+        type: Boolean,
+        default: false
+    })
+    disabled: boolean;
+
+    @Prop({
+        type: Array,
+        default: (): [] => {
+            return [];
+        }
+    })
+    options: Array<IModel>;
+
+    @Prop({
+        type: Boolean,
+        default: false
+    })
+    searchable: boolean;
+
+    @Prop({
+        type: Boolean,
+        default: false
+    })
+    required: boolean;
+
+    @Prop({
+        type: Function
+    })
+    selectable: (option: any) => boolean;
+
+    @Prop({
+        type: Function
+    })
+    showOption: (option: any) => string;
+
+    @Prop({
+        type: String
+    })
+    initError: string;
+
+    public internalValue: IModel | string = null;
+    public isDirty = false;
+    private initialError: string = null;
+    private isTouched = false;
+
+    // ToDo Matthias
+    public created() {
+        this.initialError = this.initError;
+
+        if (this.initValue) {
+            this.internalValue = this.initValue;
+        } else {
+            this.internalValue = this.value;
+        }
+    }
+
+    public beforeUpdate() {
+        if (this.isBlade) {
+            return;
+        }
+        this.internalValue = this.calcValue;
+    }
+
+    get calcValue() {
+        return this.value;
+    }
+
+    @Emit()
+    search(search: any, loading: any) {
+        return { search, loading };
+    }
+
+    @Emit()
+    blur(ev: any) {
+        this.isTouched = true;
+        return ev;
+    }
 }
 </script>

140a108e179cf0490aaecbd7128299bde4b1655d
Sil3nce72 <nb.guen@hotmail.com>
2021-09-30 09:11:45 +0200

    Ãvorherige anpassungen uebernommen

--- resources/views/components/user/current-schedule.blade.php
+++ resources/views/components/user/current-schedule.blade.php
@@ -3,18 +3,20 @@
     <x-slot name="title">
         @lang('l.scheduleCurrent')
     </x-slot>
-    @for($i = 1; $i < 6; $i++)
-        <div class="flex flex-col mb-4">
-            <div class="font-bold">
-                {{ $i }}. Semester
-            </div>
 
-            @foreach($modules as $module)
-                <div class="bg-gray-200 p-2 m-2 rounded">
-                    Modul {{ $module }}
+    <div class="md:grid md:grid-cols-2 md:gap-x-6 md:gap-y-3">
+        @for($i = 1; $i < 5; $i++)
+            <div class="flex flex-col mb-4">
+                <div class="font-bold mb-2">
+                    {{ $i }}. Semester
                 </div>
-            @endforeach
-        </div>
-    @endfor
+                @foreach($modules as $module)
+                    <div class="bg-gray-200 p-2 m-2 rounded">
+                        {{ $module }}
+                    </div>
+                @endforeach
+            </div>
+        @endfor
+    </div>
 </x-app.card>
 

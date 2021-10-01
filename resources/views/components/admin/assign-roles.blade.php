e273d5007e75db8120d15a425b37f62ff18da54d
Sil3nce72 <nb.guen@hotmail.com>
2021-10-01 08:22:47 +0200

    anpassungen assign-roles

--- resources/views/components/admin/assign-roles.blade.php
+++ resources/views/components/admin/assign-roles.blade.php
@@ -5,10 +5,12 @@
     <div class="text-center">
         <vue-form>
             @csrf
-            <div class="flex flex-col space-y-6">
-                <x-base.input type="text" name="user" label="User" value="{{ old('user') }}" :init-errors="$errors->get('user')"/>
-                <x-base.select clearable name="select" optionKey="name" label="select"></x-base.select>
-                <x-base.button class="mt-4 button-primary w-36 justify-center" type="submit">@lang('l.save')</x-base.button>
+            <div class="flex flex-row space-x-8">
+                <x-base.input class="my-auto" type="text" name="user" label="User" value="{{ old('user') }}" :init-errors="$errors->get('user')"/>
+                <x-base.select class="my-auto" clearable name="select" optionKey="name" label="select"></x-base.select>
+            </div>
+            <div class="flex justify-start mt-7">
+                <x-base.button class="button-primary w-36" type="submit">@lang('l.save')</x-base.button>
             </div>
         </vue-form>
     </div>

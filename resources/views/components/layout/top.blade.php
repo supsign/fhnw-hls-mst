08a8b77281a0b582d583dcf2167f4df551c44988
Sil3nce72 <nb.guen@hotmail.com>
2021-10-04 09:28:37 +0200

    header

--- resources/views/components/layout/top.blade.php
+++ resources/views/components/layout/top.blade.php
@@ -2,7 +2,7 @@
     <div class="container flex flex-col md:flex-row justify-between mx-auto px-3">
         <div class="flex">
             <a href="{{ route('home') }}">
-                <img class="p-2 md:p-4 max-h-20" src="{{ asset('img/logos/fhnw-logo-klein.png') }}" alt="Logo FHNW">
+                <img class="p-2 md:p-4 max-h-12 md:max-h-20" src="{{ asset('img/logos/fhnw-logo-klein.png') }}" alt="Logo FHNW">
             </a>
             <div class="my-auto text-lg">Modul Selektionstool</div>
         </div>

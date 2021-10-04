712bd1f3d0b91e076b69eda96dc9d865cfa1a467
Sil3nce72 <nb.guen@hotmail.com>
2021-10-04 11:28:07 +0200

    userdata meblade rework

--- resources/views/components/user/userdata.blade.php
+++ resources/views/components/user/userdata.blade.php
@@ -1,15 +1,23 @@
 <div>
     <x-app.card>
-        <x-slot name="title">
-            @lang('l.userData')
-        </x-slot>
-
         <!-- ToDo mehr Angaben zum Standartuser hinzufügen -->
-        <div>
-            <div>@lang('l.userName') {{ $user->firstname }} {{ $user->lastname }}</div>
-            <div>@lang('l.role') </div>
-            <div>@lang('l.studyField')</div>
-            <div>@lang('l.studyProgram')</div>
+        <div class="divide-y">
+            <div class="pb-3">
+                <div>{{ $user->firstname }} {{ $user->lastname }}</div>
+                @if($user->role !== 'student')
+                    <div>@lang('l.role'):</div>
+                @endif
+            </div>
+
+            <div class="py-3">
+                <div>@lang('l.studyField'):</div>
+                <div>@lang('l.studyProgram'):</div>
+                <div>@lang('l.term'):</div>
+                <div>@lang('l.ects'):</div>
+                <div class="mt-4">
+                    <a href="{{ route('user.index') }}" class="button-primary ">Details</a>
+                </div>
+            </div>
         </div>
     </x-app.card>
 </div>

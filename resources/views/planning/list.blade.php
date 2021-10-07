8731c8f1d79144ebf9a259664d50f14b65602615
Sil3nce72 <nb.guen@hotmail.com>
2021-10-07 08:31:34 +0200

    letzte anpassungen

--- resources/views/planning/list.blade.php
+++ resources/views/planning/list.blade.php
@@ -1,29 +1,22 @@
 <x-layout.app>
     <x-slot name="title">
-        neuer Stundenplan
+        Studienplanung
     </x-slot>
 
     <div class="container p-3 mx-auto">
         <x-app.card>
             <x-slot name="title">
-                neue Studienplanung
+                Studienplanung
             </x-slot>
-            <div class="border-2 mt-4 p-4 rounded ">
-                <div class="float-right md:float-none p-4 md:px-2">
-                    <i class="far fa-lightbulb fa-4x" aria-hidden="true"></i>
-                </div>
-                <div class="text-justify">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                    ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-                    dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-                    amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                    ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-                    dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-                    amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
-                    ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
-                    dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-                    amet.
-                </div>
+            <div class="border-2 mt-4 p-4 rounded text-lg">
+                <vue-form>
+                    <div class="py-2 px-4 bg-red-500 rounded-md text-sm text-white float-right">
+                        <span class="">Delete</span>
+                    </div>
+                    <div>{{ $planning->studyFieldYear->studyField->studyProgram->name }}</div>
+                    <div>{{ $planning->studyFieldYear->studyField->name }}</div>
+                    <div>@lang('l.startDate'): {{ $planning->studyFieldYear->beginSemester->year }}</div>
+                </vue-form>
             </div>
         </x-app.card>
     </div>

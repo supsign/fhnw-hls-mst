eebd2d96b5c496e0c449e500ed3570c1d81b282b
Sil3nce72 <nb.guen@hotmail.com>
2021-10-13 15:07:21 +0200

    anpassung

--- resources/vue/components/planning/vueCourseGroupState.vue
+++ resources/vue/components/planning/vueCourseGroupState.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-1/3 md:w-auto">
-        <div class="border p-2 md:w-full p-2 mx-auto">{{ countCredits }} / {{ courseGroupYear.credits_to_pass }}</div>
+        <div class="border p-2 md:w-full p-2 mx-auto">{{ countCredits }}&nbsp;/&nbsp;{{ courseGroupYear.credits_to_pass }}</div>
     </div>
 </template>
 

1fc462b5a72e52b203453b4f0d34b95df6ca5c06
Florian Ratz <florian.ratz@supsign.ch>
2021-10-14 14:46:02 +0200

    clear test controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,13 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Imports\CourseExcelImport;
-use Maatwebsite\Excel\Excel;
-
 class TestController extends Controller
 {
-    public function test(Excel $excel)
+    public function test()
     {
-        $excel->import(new CourseExcelImport, 'Tab3_Modul.xlsx');
     }
 }

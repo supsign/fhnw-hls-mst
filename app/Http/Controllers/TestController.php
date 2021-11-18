b2e1d1cf7a856776480b05ddb7416ee890fe0d92
Florian Ratz <florian.ratz@supsign.ch>
2021-11-18 15:24:57 +0100

    not you

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Imports\CrossQualificationImport;
-
 class TestController extends Controller
 {
     public function test()
     {
-        (new CrossQualificationImport)->import();
     }
 }

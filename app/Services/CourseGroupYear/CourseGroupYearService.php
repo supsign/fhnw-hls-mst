2da93c3874e8acf3359df6e6c69e9803357e0e3c
Florian Ratz <florian.ratz@supsign.ch>
2021-11-24 09:49:43 +0100

    courseGroupYearIsSuccessfullyCompleted

--- app/Services/CourseGroupYear/CourseGroupYearService.php
+++ app/Services/CourseGroupYear/CourseGroupYearService.php
@@ -3,15 +3,30 @@
 namespace App\Services\CourseGroupYear;
 
 use App\Models\CourseGroupYear;
+use App\Models\Student;
 use App\Services\Base\BaseModelService;
 use App\Services\Base\Traits\UpdateOrCreateTrait;
+use App\Services\Completion\CourseCompletionService;
 
 class CourseGroupYearService extends BaseModelService
 {
     use UpdateOrCreateTrait;
 
-    public function __construct(protected CourseGroupYear $model)
+    public function __construct(protected CourseGroupYear $model, protected CourseCompletionService $courseCompletionService)
     {
         parent::__construct($model);
+    }
+
+    public function courseGroupYearIsSuccessfullyCompleted(CourseGroupYear $courseGroupYear, Student $student): bool
+    {
+        foreach ($courseGroupYear->courses AS $course) {
+            if ($this->courseCompletionService->courseIsSuccessfullyCompleted($course, $student)) {
+                continue;
+            }
+
+            return false;
+        }
+
+        return true;
     }
 }

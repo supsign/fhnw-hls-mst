d544f8c99a484f5eebdd2add34112070e2a835e3
Fabrizio Cocco <fabcocco@users.noreply.github.com>
2021-11-24 16:02:24 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/PlanningController.php
+++ app/Http/Controllers/PlanningController.php
@@ -26,12 +26,11 @@ class PlanningController extends Controller
 {
     public function __construct(
         private PermissionAndRoleService $permissionAndRoleService,
-        protected StudyFieldService      $studyFieldService,
-        protected SemesterService        $semesterService,
-        protected PlanningService        $planningService,
-        protected StudyFieldYearService  $studyFieldYearService,
-    )
-    {
+        protected StudyFieldService $studyFieldService,
+        protected SemesterService $semesterService,
+        protected PlanningService $planningService,
+        protected StudyFieldYearService $studyFieldYearService,
+    ) {
     }
 
     public function create()
@@ -61,13 +60,12 @@ class PlanningController extends Controller
     }
 
     public function store(
-        StoreRequest                  $request,
-        SpecializationYearService     $specializationYearService,
-        SpecializationService         $specializationService,
-        CrossQualificationService     $crossQualificationService,
+        StoreRequest $request,
+        SpecializationYearService $specializationYearService,
+        SpecializationService $specializationService,
+        CrossQualificationService $crossQualificationService,
         CrossQualificationYearService $crossQualificationYearService
-    )
-    {
+    ) {
         $this->permissionAndRoleService->canPlanScheduleOrAbort();
 
         $studyFieldYear = $this->studyFieldYearService->getByStudyFieldIdAndSemesterId(

c4c66912ec0673dfba9e26d5426dca9132f415b4
Fabrizio Cocco <fabrizio.cocco@supsign.ch>
2021-11-25 07:35:42 +0100

    added functions

--- resources/vue/components/planning/vuePlanWrapper.vue
+++ resources/vue/components/planning/vuePlanWrapper.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="p-2 bg-white rounded shadow mb-4">
-        <div class="content-center text-base" @click="toggleCollapse">
+  <div class="p-2 bg-white rounded shadow mb-4">
+    <div class="content-center text-base" @click="toggleCollapse">
 
-            <div class="flex flex-row justify-between space-x-3">
-                <div class="my-auto">
-                    <i v-if="isCollapsed" aria-hidden="true" class="fas fa-arrow-right"></i>
-                    <i v-if="!isCollapsed" aria-hidden="true" class="fas fa-arrow-down"></i>
-                </div>
-                <slot name="header"></slot>
-            </div>
+      <div class="flex flex-row justify-between space-x-3">
+        <div class="my-auto">
+          <i v-if="isCollapsed" aria-hidden="true" class="fas fa-arrow-right"></i>
+          <i v-if="!isCollapsed" aria-hidden="true" class="fas fa-arrow-down"></i>
+        </div>
+        <slot name="header"></slot>
+      </div>
 
-        </div>
-        <div v-if="!isCollapsed" class="pt-2 pb-0 text-sm lg:text-base">
-            <slot></slot>
-        </div>
     </div>
+    <div v-if="!isCollapsed" class="pt-2 pb-0 text-sm lg:text-base">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -23,13 +23,35 @@ import BaseComponent from "../base/baseComponent";
 
 @Component
 export default class VuePlanWrapper extends BaseComponent {
-    public isCollapsed = true;
+  public isCollapsed = true;
+  public break = 1024;
 
-    public toggleCollapse() {
-        this.isCollapsed = !this.isCollapsed;
+  public toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  public expand() {
+    this.isCollapsed = false;
+  }
+
+  public handlerResize() {
+    if (window.innerWidth > this.break) {
+      this.expand();
     }
+  }
+
+  public mounted() {
+    window.addEventListener('resize', this.handlerResize);
+    this.handlerResize()
+  }
+
+  public destroyed() {
+    window.removeEventListener('resize', this.handlerResize);
+  }
 
 
 }
+
+
 </script>
 

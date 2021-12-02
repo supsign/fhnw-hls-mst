d74f424f2d1fb56cdd9e83af8c0cd5ee5cf2b811
Florian Ratz <florian.ratz@supsign.ch>
2021-12-02 09:14:13 +0100

    server admin seeder

---
+++ database/seeders/ServerAdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Services\User\PermissionAndRoleService;
+use Illuminate\Database\Seeder;
+
+class ServerAdminSeeder extends Seeder
+{
+    public function __construct(protected PermissionAndRoleService $permissionAndRoleService)
+    {
+        
+    }
+
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::firstOrCreate([
+            'email_hash' => '5ab7586c41ff8b3911b44438e9374c2819c64deb08659406e8e89b0d9d00c94588e46714af30dda96c99065d77add8c0b2b182ba865cef75df89119edd0509a0',
+        ]);
+
+        $user->assignRole('server-admin');
+    }
+}

dea03e42aad4b63e69a7cce54e26ab928ca8285f
Florian Ratz <florian.ratz@supsign.ch>
2021-12-03 08:39:58 +0100

    add mentors to seeder

--- database/seeders/MentorSeeder.php
+++ database/seeders/MentorSeeder.php
@@ -14,8 +14,11 @@ class MentorSeeder extends Seeder
      */
     public function run(MentorService $mentorService)
     {
-        $mentorService->createOrUpdateOnEventoPersonId(1, 'Müller', 'Till');
-        $mentorService->createOrUpdateOnEventoPersonId(2, 'Meier', 'Max');
-        $mentorService->createOrUpdateOnEventoPersonId(3, 'Berger', 'Anna');
+        $mentorService->createOrUpdateOnEventoPersonId(453149, 'Degen', 'Markus');
+        $mentorService->createOrUpdateOnEventoPersonId(764996, 'Hemm-Ode', 'Simone');
+        $mentorService->createOrUpdateOnEventoPersonId(928678, 'Germershaus', 'Oliver');
+        $mentorService->createOrUpdateOnEventoPersonId(10075603, 'Langer', 'Miriam');
+        $mentorService->createOrUpdateOnEventoPersonId(7631, 'Schlotterbeck', 'Götz');
+        $mentorService->createOrUpdateOnEventoPersonId(889620, 'Suter-Dick', 'Laura');
     }
 }

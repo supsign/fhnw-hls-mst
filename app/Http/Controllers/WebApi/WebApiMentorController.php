d3ec62ca8262e2928371d95777a58f13f90ea58b
Fabrizio Cocco <fabcocco@users.noreply.github.com>
2021-12-10 16:05:22 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/WebApi/WebApiMentorController.php
+++ app/Http/Controllers/WebApi/WebApiMentorController.php
@@ -17,10 +17,9 @@ use Illuminate\Support\Facades\Request;
 class WebApiMentorController extends Controller
 {
     public function __construct(
-        protected MentorService            $mentorService,
+        protected MentorService $mentorService,
         protected PermissionAndRoleService $permissionAndRoleService,
-    )
-    {
+    ) {
     }
 
     public function attachToStudent(Mentor $mentor, Student $student, AttachStudentToMentorService $attacheStudentToMentorService): MentorStudent
@@ -48,7 +47,6 @@ class WebApiMentorController extends Controller
 
         return $attacheStudentToMentorService->attach($mentor, $user->student);
     }
-
 
     public function detachStudent(Mentor $mentor, AttachStudentToMentorService $attacheStudentToMentorService): void
     {

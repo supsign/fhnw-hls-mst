98bf4ec1568c69b41023bea7fa78796568a01690
Sil3nce72 <nb.guen@hotmail.com>
2021-12-13 15:27:10 +0100

    strich bei credits raus

--- resources/views/components/user/studentdata.blade.php
+++ resources/views/components/user/studentdata.blade.php
@@ -2,7 +2,7 @@
     <div>{{ $student->studyFieldYear->studyField->studyProgram->name ?? __("l.studyField") . ': -' }}</div>
     <div>{{ $student->studyFieldYear->studyField->name ?? __("l.studyProgram") .  ': -' }}</div>
     <div>{{ $student->studyFieldYear->beginSemester->year ?? __("l.term") .  ': -' }}</div>
-    <div>@lang('l.credits'): {{ $studentCredits ?? '-'}}</div>
+    <div>@lang('l.credits'): {{ $studentCredits}}</div>
     @if(isset($student->studyFieldYear))
         <div class="mt-4 flex md:flex-none text-center">
             <a href="{{route('standing.index')}}" class="button-primary md:w-auto">@lang('l.currentStatus')</a>

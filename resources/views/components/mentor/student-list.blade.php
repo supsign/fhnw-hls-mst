0c10693558d4307e13b5ddfc80514e5d4f88183a
Sil3nce72 <nb.guen@hotmail.com>
2021-12-13 15:25:37 +0100

    erster wurf

--- resources/views/components/mentor/student-list.blade.php
+++ resources/views/components/mentor/student-list.blade.php
@@ -2,18 +2,23 @@
     <x-slot name="title">
         <div class="flex flex-row justify-between">
             <div class="my-auto">
-                Student:innen
+                Studierende
             </div>
         </div>
     </x-slot>
-
-    <div class="grid grid-cols-1 sm:grid-cols-2 sm:gap-4 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mb-4">
-        @foreach($mentorStudents  as $mentorStudent)
-            <a href="{{ route('mentor.planning.list', $mentorStudent->student) }}">
-                <div class="p-2 md:p-3 bg-gray-100 mb-3 rounded text-sm md:text-base">
-                    <div>{{ $mentorStudent->firstname }} {{ $mentorStudent->lastname }}</div>
-                </div>
-            </a>
-        @endforeach
-    </div>
+    @if(!empty($mentorStudents))
+        <div class="grid grid-cols-1 sm:grid-cols-2 sm:gap-4 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mb-4">
+            @foreach($mentorStudents  as $mentorStudent)
+                <a href="{{ route('mentor.planning.list', $mentorStudent->student) }}">
+                    <div class="p-2 md:p-3 bg-gray-100 mb-3 rounded text-sm md:text-base">
+                        <div>{{ $mentorStudent->firstname }} {{ $mentorStudent->lastname }}</div>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+    @else
+        <div>
+            Kein Student:inn hat bisher eine Planung für Sie freigegeben.
+        </div>
+    @endif
 </x-app.card>

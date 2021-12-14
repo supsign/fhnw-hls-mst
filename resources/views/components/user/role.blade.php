6f88105c4ef31f52b8dce04dcbe3f7a3d87abfeb
Sil3nce72 <nb.guen@hotmail.com>
2021-12-14 09:43:29 +0100

    kleine anpassungen

--- resources/views/components/user/role.blade.php
+++ resources/views/components/user/role.blade.php
@@ -1,8 +1,6 @@
-@if($roles)
-    <div class="flex space-x-2">
-        <div>@lang('l.roles'):</div>
-        @foreach($roles as $role)
-            <div>{{ $role }} </div>
-        @endforeach
-    </div>
-@endif
+<div class="flex space-x-2">
+    <div>@lang('l.roles'):</div>
+    @foreach($roles as $role)
+        <div>{{ $role }} </div>
+    @endforeach
+</div>

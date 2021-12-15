3a0752f65be0d012098269c1489ed19c0c26484f
Matthias Baumgartner <matthias@zehnders.ch>
2021-12-15 09:38:27 +0100

    get Role by Email Whitelisting

--- app/Services/Auth/RoleService.php
+++ app/Services/Auth/RoleService.php
@@ -7,29 +7,62 @@ use Exception;
 
 class RoleService
 {
+    private $admins = [
+        'beat.zehnder@fhnw.ch',
+        'lilian.gilgen@fhnw.ch',
+        'andreas.seelig@fhnw.ch',
+        'frank.pude@fhnw.ch'
+
+    ];
+    private $mentors = [
+        'miriam.langer@fhnw.ch',
+        'markus.degen@fhnw.ch',
+        'goetz.schlotterbeck@fhnw.ch',
+        'oliver.germershaus@fhnw.ch'
+    ];
+    private $students = [
+        'britta.brugger@students.fhnw.ch',
+        'nadja.flueckiger@students.fhnw.ch',
+        'lukas.ackermann@students.fhnw.ch',
+        'talea.marty@students.fhnw.ch',
+        'tabea.grossenbacher@students.fhnw.ch',
+        'luca.anderhub@students.fhnw.ch',
+        'jana.leonhardt@students.fhnw.ch',
+        'adam.kummer@students.fhnw.ch',
+        'tiffany.moraz@students.fhnw.ch',
+        'jonas.hauswurz@students.fhnw.ch',
+        'gianluca.frongia@students.fhnw.ch',
+        'tamara.ammonkircher@students.fhnw.ch',
+        'gentian.bilalli@students.fhnw.ch',
+        'malte.probst@students.fhnw.ch',
+        'philippe.langer@students.fhnw.ch',
+        'antonia.luethi@students.fhnw.ch',
+        'benedikt.broennimann@students.fhnw.ch',
+        'keanu.werder@students.fhnw.ch',
+        'sebastian.stalder@students.fhnw.ch',
+        'studenthls.iamtest@students.fhnw.ch',
+        'andreas.seelig@students.fhnw.ch'
+    ];
+
     public function evaluate(ShibbolethProperties $shibbolethProperties): string
     {
-        if (!$shibbolethProperties->mail || !$shibbolethProperties->entitlement) {
+        if (!$shibbolethProperties->mail) {
             throw new Exception('no authorized role identified');
         }
 
-        $shibbolethProperties->entitlement = strtolower($shibbolethProperties->entitlement);
-        $staticPartOfUrl = 'http://fhnw.ch/aai/res/hls/stab/';
-
-        switch (true) {
-            case str_contains($shibbolethProperties->entitlement, $staticPartOfUrl.'mst_admin'):
-                return 'app-admin';
-
-            case str_contains($shibbolethProperties->entitlement, $staticPartOfUrl.'mst_stgl'):
-            case str_contains($shibbolethProperties->entitlement, $staticPartOfUrl.'mst_mentor'):
-                return 'mentor';
-
-            case str_contains($shibbolethProperties->entitlement, $staticPartOfUrl.'mst_adm_student'):
-            case str_contains($shibbolethProperties->entitlement, $staticPartOfUrl.'mst_edu_student'):
-                return 'student';
-
-            default:
-                throw new Exception('no authorized role identified');
+        if (in_array($shibbolethProperties->mail, $this->admins)) {
+            return 'app-admin';
         }
+
+        if (in_array($shibbolethProperties->mail, $this->mentors)) {
+            return 'mentor';
+        }
+
+        if (in_array($shibbolethProperties->mail, $this->students)) {
+            return 'student';
+        }
+
+        throw new Exception('no authorized role identified');
+
     }
 }

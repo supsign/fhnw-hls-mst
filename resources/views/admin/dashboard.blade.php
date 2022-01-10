859b2e1c36291b3ebc6b5dcec0d04906f29890b9
Florian Ratz <florian.ratz@supsign.ch>
2022-01-10 15:23:26 +0100

    add to menu

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,12 +3,12 @@
         @lang('l.dashboard')
     </x-slot>
 
-        <x-app.card class="flex flex-col overflow-hidden w-full rounded-none">
-            <x-slot name="title">
-                @lang('l.dashboard')
-            </x-slot>
-            <div class="text-center">
-                <span class="font-heading text-3xl">You are Awesome and at least a super-admin!</span>
-            </div>
-        </x-app.card>
+    <x-app.card class="flex flex-col overflow-hidden w-full rounded-none">
+        <x-slot name="title">
+            @lang('l.dashboard')
+        </x-slot>
+        <div class="text-center">
+            <span class="font-heading text-3xl">You are Awesome and at least a super-admin!</span>
+        </div>
+    </x-app.card>
 </x-layout.admin>

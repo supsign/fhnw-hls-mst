40a94cb96f24df8577910298db7828bb8dde1210
Shift <shift@laravelshift.com>
2022-06-17 11:50:09 +0000

    Remove unnecessary `$model` property

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -9,13 +9,6 @@ use Illuminate\Support\Facades\Hash;
 class StudentFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Student::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array

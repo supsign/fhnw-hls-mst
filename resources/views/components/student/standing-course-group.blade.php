1fe7b3e5468b2fdf8231104d6319db979300ea64
Florian Ratz <florian.ratz@supsign.ch>
2022-06-23 16:40:53 +0200

    no message

--- resources/views/components/student/standing-course-group.blade.php
+++ resources/views/components/student/standing-course-group.blade.php
@@ -1,9 +1,9 @@
-<x-app.card completed="{{$completed}}">
+<x-app.card completed="{{ $completed }}">
     <x-slot name="title">
         @if($completed)
             <i class="far fa-check-circle text-green-500 font-bold" aria-hidden="true"></i>
         @endif
-        {{$courseGroupYear->courseGroup->name}}
+        {{ $courseGroupYear->courseGroup->name }}
     </x-slot>
     @foreach($courseGroupYear->courses as $course)
         <x-student.standing-course :student="$student" :course="$course"/>
@@ -11,7 +11,7 @@
 
     <x-slot name="footer">
         <div class="flex justify-end">
-            <div>Total: {{$reachedCredits}}/{{$courseGroupYear->credits_to_pass}}</div>
+            <div>Total: {{ $reachedCredits }}/{{ $courseGroupYear->credits_to_pass }}</div>
         </div>
     </x-slot>
 

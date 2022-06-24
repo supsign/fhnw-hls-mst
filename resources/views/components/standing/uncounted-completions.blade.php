7b7474706a8260a7d504f4f1a3c91ae576d27de7
Florian Ratz <florian.ratz@supsign.ch>
2022-06-24 13:42:33 +0200

    no message

--- resources/views/components/standing/uncounted-completions.blade.php
+++ resources/views/components/standing/uncounted-completions.blade.php
@@ -2,9 +2,12 @@
     <x-slot name="title">
         Weitere abgeschlossene Module
     </x-slot>
+
     @foreach($completions as $completion)
-        <vue-uncounted-completion-course :student="{{$student}}" :completion="{{$completion}}" :course-groups="{{$student->studyFieldYear?->courseGroups}}"/>
+        <vue-uncounted-completion-course 
+            :student="{{ $student }}" 
+            :completion="{{ $completion }}" 
+            :course-groups="{{ $student->studyFieldYear?->courseGroups }}"
+        />
     @endforeach
-
-
 </x-app.card>
